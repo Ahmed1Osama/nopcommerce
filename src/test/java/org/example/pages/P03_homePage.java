199b67e6008b4de6d063ebb856e2b4d0ef99aa22
Ahmed1Osama <39135931+Ahmed1Osama@users.noreply.github.com>
2022-08-13 05:13:48 +0200

    firstAuto

--- src/test/java/org/example/pages/P03_homePage.java
+++ src/test/java/org/example/pages/P03_homePage.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class P03_homePage {
     public P03_homePage() {
@@ -33,8 +37,24 @@ public class P03_homePage {
     @FindBy(className = "sku")
     public WebElement serilProduct;
 
+    @FindBy(xpath= "/html/body/div[6]/div[2]/ul[1]") ////ul[@class=\"top-menu notmobile\"]/li"
+    public List<WebElement> maincategories;               // /html/body/div[6]/div[2]/ul[1]
+                                                         // /html/body/div[6]/div[2]/ul[1]/li
 
 
+    @FindBy(className = "page-title")
+    public WebElement pageTitle;
 
 
+    @FindBy(className = "nivoSlider")
+    public WebElement slider;
+
+
+    public void implicitWait(){
+        Hooks.driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+    }
+    public void implicitWaitUntill(String s, String style, String s1) {
+        WebDriverWait wait = new WebDriverWait(Hooks.driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.attributeToBe(By.xpath(s), style, s1));
+    }
 }
